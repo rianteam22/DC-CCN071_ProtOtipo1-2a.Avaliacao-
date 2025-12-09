0fde182b732abbb090daeb28daed72ef3e97bd12
rianteam22 <rian.team22@gmail.com>
2025-12-09 08:40:45 -0300

    fix: Update error response handling in token authentication

--- Backend/index.js
+++ Backend/index.js
@@ -44,19 +44,19 @@ app.use(express.static(path.join(__dirname, '..', 'FrontEnd')));
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
-  
+
   if (!token){
-    return res.sendStatus(401).json({ error: 'Token não fornecido' });
+    return res.status(401).json({ error: 'Token não fornecido' });
   }
-  
+
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err){
-      return res.sendStatus(403).json({ error: 'Token inválido' });
+      return res.status(403).json({ error: 'Token inválido' });
     }
-    
+
     req.user = user;
     next();
-    
+
   });
 }
 
